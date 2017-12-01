669d62a43a6997a01a8e60be3cc47fadd5634796
Calle Svensson <calle.svensson@zeta-two.com>
2017-12-01 20:59:15 +0100

    Better version of Day01

--- src/main/scala/com/zetatwo/Day01.scala
+++ src/main/scala/com/zetatwo/Day01.scala
@@ -4,29 +4,24 @@ object Day01 {
   def main(args: Array[String]): Unit = {
     val input = io.StdIn.readLine()
 
-    printf("Result: %d", sum2(input))
+    printf("Result 1: %d", sum(input))
+    printf("Result 2: %d", sum2(input))
+  }
+
+  def doSum(input: String, rotation: Int): Int = {
+    val digits: Seq[Int] = input.map(c => Integer.valueOf(c.toString).intValue())
+    val rotated: Seq[Int] = digits.slice(rotation, digits.length) ++ digits.slice(0, rotation)
+    val pairs: Seq[(Int, Int)] = digits zip rotated
+    val pairvalues: Seq[Int] = pairs.flatMap(p => if (p._1 == p._2) List(p._1) else List())
+    pairvalues.sum
   }
 
   def sum(input: String): Int = {
-    val right: String = input.substring(1) + input.charAt(0)
-    val left: String = input
-    val pairs: Seq[(Char, Char)] = left zip right
-    val pairvalues: Seq[Int] = {
-      pairs.map(p => if (p._1 == p._2) Integer.valueOf(p._1.toString).intValue() else 0)
-    }
-
-    pairvalues.sum
+    doSum(input, 1)
   }
 
   def sum2(input: String): Int = {
-    val right: String = input.substring(input.length/2) + input.substring(0, input.length/2)
-    val left: String = input
-    val pairs: Seq[(Char, Char)] = left zip right
-    val pairvalues: Seq[Int] = {
-      pairs.map(p => if (p._1 == p._2) Integer.valueOf(p._1.toString).intValue() else 0)
-    }
-
-    pairvalues.sum
+    doSum(input, input.length/2)
   }
 
 }
