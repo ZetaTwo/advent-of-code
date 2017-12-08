4dedd9574abc123dc7148ca94118e68c030b0a48
Calle Svensson <calle.svensson@zeta-two.com>
2017-12-08 02:55:51 +0100

    Solved Day07

--- src/main/scala/com/zetatwo/Common.scala
+++ src/main/scala/com/zetatwo/Common.scala
@@ -1,6 +1,7 @@
 package com.zetatwo
 
 object Common {
+  // Measure the execution time of a block
   def time[R](block: => R): R = {
     val t0 = System.nanoTime()
     val result = block // call-by-name
@@ -8,4 +9,10 @@ object Common {
     println("Elapsed time: " + (t1 - t0) + "ns")
     result
   }
+
+  // Transform Seq[Either[A, B]] -> Either[A, Seq[B]]
+  def sequence[A, B](s: Seq[Either[A, B]]): Either[A, Seq[B]] =
+    s.foldRight(Right(Nil): Either[A, List[B]]) {
+      (e, acc) => for (xs <- acc.right; x <- e.right) yield x :: xs
+    }
 }
